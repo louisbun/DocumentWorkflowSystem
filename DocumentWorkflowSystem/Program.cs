5d8250f4441036d099d0af817e681d7e603be796
JosephFed <S10241860@connect.np.edu.sg>
2025-02-03 11:39:38 +0800

    observer pattern skeleton

--- DocumentWorkflowSystem/Program.cs
+++ DocumentWorkflowSystem/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DocumentWorkflowSystem;
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
