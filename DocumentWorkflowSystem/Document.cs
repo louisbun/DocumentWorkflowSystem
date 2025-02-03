fc73eae18fdd63e0bb5586c587672757008f16db
JosephFed <S10241860@connect.np.edu.sg>
2025-02-03 11:09:28 +0800

    Document class

---
+++ DocumentWorkflowSystem/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentWorkflowSystem
+{
+    internal class Document
+    {
+        //protected ConvertBehavior convertBehavior; (Strategy pattern)
+        private string documentID;
+        private string title;
+        //private DocumentType type; (State Pattern)
+        private string content;
+        private string header;
+        private string footer; 
+    }
+}
